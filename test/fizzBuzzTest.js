c49b65d338df141a163f70271b4d6071beba325e
Brittany Lacoste <lacostebrittany0227@gmail.com>
2017-08-03 18:23:28 -0700

    Tests almost done

--- test/fizzBuzzTest.js
+++ test/fizzBuzzTest.js
@@ -1,9 +1,14 @@
 const assert = require('chai').assert;
 const expect = require('chai').expect;
-const app = require('../fizzBuzz.js');
+const fizzBuzz = require('../fizzBuzz.js');
 
-describe.only('fizzBuzz', function() {
-    it('should exist', function() {
-        expect(fizzBuzz).to.exist; 
+describe.only('fizzBuzz', function () {
+    describe('Smoke tests', function () {
+        it('should exist', function () {
+            expect(fizzBuzz).to.exist;
+        })
+        it('Should be a function', function() {
+            expect(fizzBuzz).to.be.a('function');
+        })
     })
 })
