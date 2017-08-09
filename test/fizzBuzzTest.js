c983374335e5ec1c0e7c5d3103569d359c49c390
Brittany Lacoste <lacostebrittany0227@gmail.com>
2017-08-09 15:21:29 -0700

    Module not defined error fixed with broswerify

--- test/fizzBuzzTest.js
+++ test/fizzBuzzTest.js
@@ -2,13 +2,18 @@ const assert = require('chai').assert;
 const expect = require('chai').expect;
 const fizzBuzz = require('../fizzBuzz.js');
 
-describe.only('fizzBuzz', function () {
+describe('fizzBuzz', function () {
     describe('Smoke tests', function () {
         it('should exist', function () {
             expect(fizzBuzz).to.exist;
         })
-        it('Should be a function', function() {
+        it('Should be a function', function () {
             expect(fizzBuzz).to.be.a('function');
+        })
+        describe('Functionality Tests', function () {
+            it('Pass param 5 should return \'BUZZ\'', function() {
+                expect(fizzBuzz(5)).to.equal('BUZZ');
+            });
         })
     })
 })
